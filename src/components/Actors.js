cd7e3db76649bd78638d7dacc159e533bc2ba0e5
Harold Torres Marino <haroldtm55@gmail.com>
2021-07-17 17:50:15 +1000

    Lab completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,14 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor,idx) => (
+        <div key={idx}>
+          <h4>Name: {actor.name}</h4>
+          <p>Movies:</p>
+          <ul>{actor.movies.map((movie,idx) => <li key={idx}>{movie}</li>)}</ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,14 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director,idx) => (
+        <div key={idx}>
+          <h4>Name: {director.name}</h4>
+          <p>Movies:</p>
+          <ul>{director.movies.map((movie,idx) => <li key={idx}>{movie}</li>)}</ul>
+        </div>
+      ))}
     </div>
   );
 }
